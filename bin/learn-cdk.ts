698fac3e2839f18b496e44a4fc9d8bb75e1ab9b0
subhash-malireddy <subhashmalireddy@gmail.com>
2025-06-22 10:31:21 +1000

    more cdk core concepts: multiple stacks and referencing, CfnOutputs,

--- bin/learn-cdk.ts
+++ bin/learn-cdk.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { LearnCdkStack } from '../lib/learn-cdk-stack';
+import * as cdk from "aws-cdk-lib";
+import { LearnCdkStack } from "../lib/learn-cdk-stack";
 
 const app = new cdk.App();
-new LearnCdkStack(app, 'LearnCdkStack');
+const stack1 = new LearnCdkStack(app, "LearnCdkStack");
+new LearnCdkStack(app, "LearnCdkStack2", {
+  createdAfterStackId: `${stack1.stackName}=>${stack1.stackId}`,
+});
--- lib/learn-cdk-stack.ts
+++ lib/learn-cdk-stack.ts
@@ -1,19 +1,63 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import {
+  CfnOutput,
+  Duration,
+  Fn,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+} from "aws-cdk-lib";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { S3 } from "aws-cdk-lib/aws-ses-actions";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import { Construct } from "constructs";
 
+interface LearnCdkStackProps extends StackProps {
+  // optional props
+  createdAfterStackId?: string;
+}
 export class LearnCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: LearnCdkStackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'LearnCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // const queue = new sqs.Queue(this, 'LearnCdkQueue', {
+    //   visibilityTimeout: Duration.seconds(300)
+    // });
+
+    // const topic = new sns.Topic(this, 'LearnCdkTopic');
+
+    // topic.addSubscription(new subs.SqsSubscription(queue));
+    if (props?.createdAfterStackId) {
+      console.log("Creating stack after stack id: ", props.createdAfterStackId);
+    }
+    const shortStackId = Fn.select(2, Fn.split("/", this.stackId));
+    const stackSuffix = Fn.select(4, Fn.split("-", shortStackId));
+    const photosBucket = new Bucket(this, "LearnCdkPhotosBucket", {
+      lifecycleRules: [
+        {
+          expiration: Duration.days(7),
+        },
+      ],
+      bucketName: `satti-photos-${stackSuffix}`,
+      removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    const topic = new sns.Topic(this, 'LearnCdkTopic');
+    new CfnOutput(this, "PhotosBucketName", {
+      key: "name",
+      value: photosBucket.bucketName,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new CfnOutput(this, "PhotosBucketDomainName", {
+      key: "domainName",
+      value: photosBucket.bucketDomainName,
+    });
+
+    if (props?.createdAfterStackId) {
+      new CfnOutput(this, "CreatedAfterStack", {
+        key: "createdAfterStack",
+        value: props.createdAfterStackId,
+      });
+    }
   }
 }
